07a1e324effa6bf445ce7c180a5c5a7fd065601c
CodeChanceCode <chancejolley@msn.com>
2023-04-25 19:52:13 +0000

    join and split

---
+++ challenge18.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    name_input = input("What is your name?")
+    date_input = input("What day of the week?")
+
+    print(f"Hello, {name_input}! ! Happy {date_input}!")
+
+    main()
+
+   
