17bc6c2312f91867fc038217114f914edd0e41d5
CodeChanceCode <chancejolley@msn.com>
2023-04-28 19:00:51 +0000

    Moving files into day5 directory

---
+++ Day5/loopchallenges/challenge61.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+for bottlecount in range(99,0,-1):
+    if bottlecount == 0:
+        print(f"0 bottles of beer on the wall! 0 bottles of beer! You take one down, pass it around!")
+        print(f"No more bottles of beer on the wall!\n")
+    else:
+        print(f"{bottlecount} bottles of beer on the wall! {bottlecount} bottles of beer! You take one down, pass it around!")
+        print(f"{bottlecount} bottles of beer on the wall!\n")
+    
+
---
+++ Day5/loopchallenges/challenge62.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+answer= input("Which farm would you like?")
+vegetables = ["carrots", "celery"]
+
+for farm in farms:
+    if answer in farm["name"]:
+         for animal in farm["agriculture"]:
+             if animal not in vegetables:
+                print(animal)
+
