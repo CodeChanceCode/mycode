db9b87f55d2b8686b5e8f9e91b54cb93999e41f8
CodeChanceCode <chancejolley@msn.com>
2023-05-04 16:58:13 +0000

    day9 morning backup

--- alta3research-flask01.py
+++ alta3research-flask01.py
@@ -8,7 +8,11 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/equipment")
+@app.route("/")
+def landing():
+    return render_template("landing.html")
+
+@app.route("/equipment", methods=["GET","POST"])
 def list_equipment():
     if request.method == "POST":
         filter_type = request.form.get("filter", "all")
@@ -20,13 +24,20 @@ def list_equipment():
     data = response.json()
 
     if filter_type == "attack":
-        filtered_equipment = [item for item in equipment if "attack" in item and item["attack"] > 0 and item["defense"] == 0]
+        filtered_equipment = [item for item in data["data"] if "attack" in item and item["attack"] > 0 and item["defense"] == 0]
     elif filter_type == "defense":
-        filtered_equipment = [item for item in equipment if "defense" in item and item["defense"] > 0 and item["attack"] == 0]
+        filtered_equipment = [item for item in data["data"] if "defense" in item and item["defense"] > 0 and item["attack"] == 0]
     else:
         filtered_equipment = data["data"]    
     
-    return render_template("equipment.html", equipment=filtered_equipment)
+    random_item = random.choice(filtered_equipment)
+
+    if filter_type == "attack":
+        return render_template("equipment.html", equipment=random_item, filter_type="attack")
+    elif filter_type == "defense":
+        return render_template("equipment.html", equipment=random_item, filter_type="defense")
+
+    return render_template("equipment.html", equipment=random_item)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=2224)
+    app.run(host="0.0.0.0",port=2224, debug=True)
